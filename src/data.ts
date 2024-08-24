0d90eda8428748a1e34e7abf06019a0c3949f20c
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-24 20:03:09 -0300

    Listing invoices

---
+++ src/data.ts
@@ -0,0 +1,133 @@
+let invoices = [
+    {
+     name: "Santa Monica",
+     number: 1995,
+     amount: "R$10,800",
+     due: "12/05/1995"
+    },
+    {
+     name: "Venice Beach",
+     number: 1996,
+     amount: "R$8,300",
+     due: "15/06/1996"
+    },
+    {
+     name: "Malibu",
+     number: 1997,
+     amount: "R$12,500",
+     due: "20/07/1997"
+    },
+    {
+     name: "Beverly Hills",
+     number: 1998,
+     amount: "R$15,000",
+     due: "25/08/1998"
+    },
+    {
+     name: "Hollywood",
+     number: 1999,
+     amount: "R$9,200",
+     due: "10/09/1999"
+    },
+    {
+     name: "Downtown LA",
+     number: 2000,
+     amount: "R$11,700",
+     due: "30/10/2000"
+    },
+    {
+     name: "Westwood",
+     number: 2001,
+     amount: "R$7,900",
+     due: "05/11/2001"
+    },
+    {
+     name: "Culver City",
+     number: 2002,
+     amount: "R$13,400",
+     due: "15/12/2002"
+    },
+    {
+     name: "Santa Clarita",
+     number: 2003,
+     amount: "R$10,000",
+     due: "22/01/2003"
+    },
+    {
+     name: "Long Beach",
+     number: 2004,
+     amount: "R$14,600",
+     due: "30/02/2004"
+    },
+    {
+     name: "Pasadena",
+     number: 2005,
+     amount: "R$9,800",
+     due: "18/03/2005"
+    },
+    {
+     name: "Glendale",
+     number: 2006,
+     amount: "R$11,200",
+     due: "25/04/2006"
+    },
+    {
+     name: "Monterey Park",
+     number: 2007,
+     amount: "R$8,700",
+     due: "12/05/2007"
+    },
+    {
+     name: "Torrance",
+     number: 2008,
+     amount: "R$12,100",
+     due: "19/06/2008"
+    },
+    {
+     name: "Redondo Beach",
+     number: 2009,
+     amount: "R$10,900",
+     due: "30/07/2009"
+    },
+    {
+     name: "Burbank",
+     number: 2010,
+     amount: "R$9,500",
+     due: "10/08/2010"
+    },
+    {
+     name: "Inglewood",
+     number: 2011,
+     amount: "R$13,000",
+     due: "25/09/2011"
+    },
+    {
+     name: "Hawthorne",
+     number: 2012,
+     amount: "R$11,800",
+     due: "12/10/2012"
+    },
+    {
+     name: "Compton",
+     number: 2013,
+     amount: "R$7,600",
+     due: "28/11/2013"
+    },
+    {
+     name: "El Segundo",
+     number: 2014,
+     amount: "R$14,200",
+     due: "15/12/2014"
+    },
+    {
+     name: "Lynwood",
+     number: 2015,
+     amount: "R$10,400",
+     due: "02/01/2015"
+    }
+ ];
+ 
+ export function getInvoices() {
+     return invoices;
+ }
+ 
