3bbab11824640dac812b860068c213047dee41a6
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-26 20:46:23 -0300

    redirecionamento para rotas quue nao existem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import ClientHome from "./routes/ClientHome";
 import Catalog from "./routes/ClientHome/Catalog";
@@ -14,9 +14,11 @@ export default function App() {
       <Route index element={<Catalog/>}/>
       <Route path="catalog" element={<Catalog/>}/>
       <Route path="product-details/:productId" element={<ProductDetails/>}>
+   
        </Route>
 
       </Route>
+      <Route path="*" element={<Navigate to ="/"/>}/>
     </Routes>
     </BrowserRouter>
   );
