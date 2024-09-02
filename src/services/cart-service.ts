5651bf5a4f7282df3f1965a796aaac12eb85fcc6
calebewerneckcouto <calebewerneck@hotmail.com>
2024-09-01 22:21:15 -0300

    decrementando items do carrinhjo

--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -16,22 +16,35 @@ export function getCart(): OrderDTO {
 export function addProduct(product: ProductDTO) {
     const cart = cartRepository.get();
     const item = cart.items.find(x => x.productId === product.id);
-    if (!item){
-        const newItem = new OrderItemDTO(product.id,1,product.name,product.price,product.imgUrl);
+    if (!item) {
+        const newItem = new OrderItemDTO(product.id, 1, product.name, product.price, product.imgUrl);
         cart.items.push(newItem);
         cartRepository.save(cart);
     }
 }
 
-export function clearCart () {
+export function clearCart() {
     cartRepository.clear();
 }
 
-export function increaseItem(productId:number){
+export function increaseItem(productId: number) {
     const cart = cartRepository.get();
-    const item = cart.items.find(x=> x.productId === productId);
-    if(item){
-        item.quantity ++;
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
+        item.quantity++;
+        cartRepository.save(cart);
+    }
+}
+
+
+export function decreaseItem(productId: number) {
+    const cart = cartRepository.get();
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
+        item.quantity--;
+        if (item.quantity < 1) {
+            cart.items = cart.items.filter(x => x.productId !== productId);
+        }
         cartRepository.save(cart);
     }
 }
