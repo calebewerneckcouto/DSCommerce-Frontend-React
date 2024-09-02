86c5dca0ad084b870d65f1d77396f3f33f52d76e
calebewerneckcouto <calebewerneck@hotmail.com>
2024-09-01 21:41:58 -0300

    adicionando produto ao carrinho

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../components/models/product";
 import * as productService from '../../../services/product-service'
+import * as cartService from '../../../services/cart-service';
 
 export default function ProductDetails() {
     const { productId } = useParams(); // Extraia o productId dos parâmetros da URL
@@ -29,6 +30,15 @@ export default function ProductDetails() {
         }
     }, []); // Adicione productId como dependência
 
+    function handleBuyClick () {
+        if(product){
+            cartService.addProduct(product);
+            navigate("/cart");
+        }
+
+          
+    }
+
     return (
         <main>
             <section id="product-details-section" className="dsc-container">
@@ -36,7 +46,10 @@ export default function ProductDetails() {
                     <ProductDetailsCard product={product} />
                 )}
                 <div className="dsc-btn-page-container">
+                    <div onClick={handleBuyClick}>
                     <ButtonPrimary text="Comprar" />
+                    </div>
+                    
                     <Link to="/">
                         <ButtonSecond text="Início" />
                     </Link>
--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -1,4 +1,5 @@
-import { OrderDTO } from "../components/models/order";
+import { OrderDTO, OrderItemDTO } from "../components/models/order";
+import { ProductDTO } from "../components/models/product";
 import * as cartRepository from '../localstorage/cart-repository';
 
 export function saveCart(cart: OrderDTO) {
@@ -7,6 +8,17 @@ export function saveCart(cart: OrderDTO) {
 }
 
 
-export function getCart () : OrderDTO {
+export function getCart(): OrderDTO {
     return cartRepository.get();
+}
+
+
+export function addProduct(product: ProductDTO) {
+    const cart = cartRepository.get();
+    const item = cart.items.find(x => x.productId === product.id);
+    if (!item){
+        const newItem = new OrderItemDTO(product.id,1,product.name,product.price,product.imgUrl);
+        cart.items.push(newItem);
+        cartRepository.save(cart);
+    }
 }
