9bce93420d8981c9113d9aa4080a9d341e5c2b49
calebewerneckcouto <calebewerneck@hotmail.com>
2024-09-01 21:29:09 -0300

    obtendo carrinho do localstorage

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,29 +1,23 @@
-import './styles.css'
+import { useEffect, useState } from 'react';
+import './styles.css';
+import * as cartService from '../../../services/cart-service';
+import { OrderDTO, OrderItemDTO } from '../../../components/models/order';
 
+const item1 = new OrderItemDTO(
+  4, 1, "Pc Gamer", 1200, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
+);
 
-const cart = {
-  items: [
-    {
-      productId: 4,
-      quantity: 1,
-      name: "Pc Gamer",
-      price: 1200,
-      imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
-    },
-    {
-      productId: 5,
-      quantity: 2,
-      name: "Rails for Dummies",
-      price: 100.99,
-      imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
-    }
-  ]
-}
+const item2 = new OrderItemDTO(
+  5, 2, "Rails for Dummies", 100.99, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
+);
 
 export default function Cart() {
+
+  const [cart,setCart] = useState<OrderDTO>(cartService.getCart());
+
   const calculateTotal = () => {
-    return cart.items.reduce((total, item) => total + (item.price * item.quantity), 0);
-  }
+    return cart.items.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
 
   return (
     <main>
---
+++ src/services/cart-service.ts
@@ -0,0 +1,12 @@
+import { OrderDTO } from "../components/models/order";
+import * as cartRepository from '../localstorage/cart-repository';
+
+export function saveCart(cart: OrderDTO) {
+    cartRepository.save(cart);
+
+}
+
+
+export function getCart () : OrderDTO {
+    return cartRepository.get();
+}
