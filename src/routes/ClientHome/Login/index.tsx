46801870906566f084fba5e004101a92679a9566
calebewerneckcouto <calebewerneck@hotmail.com>
2024-09-23 16:29:57 -0300

    requisicao login 2

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import './styles.css'
 import { CredentialsDTO } from '../../../components/models/auth';
-import { loginRequest } from '../../../services/auth-service';
+
+import * as authService from '../../../services/auth-service';
 
 export default function Login() {
 
@@ -12,15 +13,21 @@ export default function Login() {
 
   function handleSubmit(event: any) {
     event.preventDefault();
-    loginRequest(formData);
+    authService.loginRequest(formData)
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.log("Erro no Login", error);
+      })
 
 
   }
 
-  function handleInputChange (event:any) {
-    const value =  event.target.value;
-    const name =  event.target.name;
-    setFormData({...formData,[name]:value})
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: value })
 
   }
 
@@ -28,28 +35,28 @@ export default function Login() {
   return (
     <main>
       <section id="login-section" className="dsc-container">
-        <div className="dsc-login-form-container" onSubmit={handleSubmit}>
-          <form className="dsc-card dsc-form">
+        <div className="dsc-login-form-container" >
+          <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
-                <input 
-                name="username"
-                value={formData.username}
-                onChange={handleInputChange}
-                className="dsc-form-control"
-                 type="text"
+                <input
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  className="dsc-form-control"
+                  type="text"
                   placeholder="Email" />
                 <div className="dsc-form-error"></div>
               </div>
               <div>
-                <input 
-                 name="password"
-                 value={formData.password}
-                 onChange={handleInputChange}
-                className="dsc-form-control"
-                 type="password" 
-                 placeholder="Senha" />
+                <input
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className="dsc-form-control"
+                  type="password"
+                  placeholder="Senha" />
               </div>
             </div>
 
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,17 +1,24 @@
 import QueryString from "qs";
 import { CredentialsDTO } from "../components/models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
+import { AxiosRequestConfig } from "axios";
+import { requestBackend } from "../utils/requests";
 
-export function loginRequest(loginData: CredentialsDTO){
-    const headers= {
-        "Content-Type" :"application/x-www-form-urlencoded",
-        Authorization :"Basic " + window.btoa(CLIENT_ID + ":"+CLIENT_SECRET) 
+export function loginRequest(loginData: CredentialsDTO) {
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
     }
 
 
-    const requestBody = QueryString.stringify({
-        ...loginData,grant_type: "password"
-    });
+    const requestBody = QueryString.stringify({...loginData, grant_type: "password" });
 
-    console.log(requestBody);
+    const config: AxiosRequestConfig = {
+        method: "POST",
+        url: "/oauth2/token",
+        data: requestBody,
+        headers
+    }
+
+    return requestBackend(config);
 }
