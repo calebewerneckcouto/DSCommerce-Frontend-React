0f74c2572be00696424f1ef441cb07f4d0fe91b9
calebewerneckcouto <calebewerneck@hotmail.com>
2024-10-01 22:21:53 -0300

    Barra navegação da area adminstrativa com NavLink

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -2,6 +2,7 @@ import './styles.css'
 import homeIcon from '../../assets/home.svg';
 import productsIcon from '../../assets/products.svg';
 import LoggedUser from '../LoggedUser'; // Importando o componente
+import { NavLink } from 'react-router-dom';
 
 export default function HeaderAdmin() {
   return (
@@ -10,14 +11,21 @@ export default function HeaderAdmin() {
         <h1>DSC Admin</h1>
         <div className="dsc-navbar-right">
           <div className="dsc-menu-items-container">
+
+            <NavLink to="/admin/home"
+            className={({isActive})=> isActive ? "dsc-menu-item-active":""}>
             <div className="dsc-menu-item">
               <img src={homeIcon} alt="Início" />
               <p>Início</p>
             </div>
+            </NavLink>
+
+            <NavLink to="/admin/products" className={({isActive})=> isActive ? "dsc-menu-item-active":""}>
             <div className="dsc-menu-item">
               <img src={productsIcon} alt="Cadastro de produtos" />
-              <p className="dsc-menu-item-active">Produtos</p>
+              <p>Produtos</p>
             </div>
+            </NavLink>
           </div>
 
           {/* Aqui utilizamos o componente LoggedUser */}
