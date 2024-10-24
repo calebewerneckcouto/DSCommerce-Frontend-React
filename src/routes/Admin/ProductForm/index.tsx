13eda6dd6363222650a7793a12c6b58d2ac8cb12
calebewerneckcouto <calebewerneck@hotmail.com>
2024-10-24 15:17:29 -0300

    inserindo um novo produto com POST

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -118,8 +118,10 @@ export default function ProductForm() {
         if (isEditing) {
             requestBody.id = params.productId;
         }
-
-        productService.updateRequest(requestBody)
+        const request = isEditing
+            ? productService.updateRequest(requestBody)
+            : productService.insertRequest(requestBody);
+        request
             .then(() => {
                 navigate("/admin/products")
             })
